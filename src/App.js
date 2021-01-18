3537123846d447a2ad5172e779313c532e6efc11
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-17 23:42:46 -0500

    Created a router and a route for Home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import Home from "./components/Home"
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
+                <h1>Bank of React</h1>
+            </div>
+        )
+    }
+}
