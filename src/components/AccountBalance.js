d9b75461bd0d7bde72757dd4e3e9ec51be8f6607
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-18 21:14:40 -0500

    added styling to the homepage

--- src/components/AccountBalance.js
+++ src/components/AccountBalance.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class AccountBalance extends Component {
     render() {
         return (
-            <div>
-                Balance: {this.props.accountBalance}
+            <div id="home-balance">
+                Your Balance: ${this.props.accountBalance}
             </div>
         )
     }
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,16 @@ import {Link} from "react-router-dom"
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                <h1>Bank of React</h1>
-                <AccountBalance accountBalance={this.props.accountBalance}/>
+            <div id="homepage">
+                <img 
+                    id="bank-img" 
+                    src="https://images.unsplash.com/photo-1518713408716-82d62758922f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=687&q=80" 
+                    alt="bank"
+                />
+                <div id="homepage-text">
+                    <h1 id="home-title">Bank of React</h1>
+                    <AccountBalance accountBalance={this.props.accountBalance}/>
+                </div>
             </div>
         )
     }
